18d517867d1d3bb298dd49ef9ee8fd6e991fe0f6
Marvin Derksen <marvin.derksen@lise.de>
2023-01-17 16:55:22 +0100

    Add buttons after on-page navigations

--- button.js
+++ button.js
@@ -1,20 +1,20 @@
-export const buttonClassName = "chrome-ext-devops-branch-name";
+export const buttonClassName = 'chrome-ext-devops-branch-name';
 
 export const createButton = (branchName) => {
-  const button = document.createElement("button");
-  button.setAttribute("title", "Copy to clipboard");
-  button.setAttribute("aria-label", "Copy to clipboard");
+  const button = document.createElement('button');
+  button.setAttribute('title', 'Copy to clipboard');
+  button.setAttribute('aria-label', 'Copy to clipboard');
   button.setAttribute(
-    "class",
+    'class',
     `bolt-button bolt-icon-button enabled icon-only bolt-focus-treatment ${buttonClassName}`
   );
-  button.setAttribute("type", "button");
-  button.setAttribute("role", "button");
-  button.setAttribute("style", buttonStyle);
+  button.setAttribute('type', 'button');
+  button.setAttribute('role', 'button');
+  button.setAttribute('style', buttonStyle);
 
-  button.appendChild(createSpan());
+  button.appendChild(createInnerSpan());
 
-  button.addEventListener("click", () =>
+  button.addEventListener('click', () =>
     navigator.clipboard.writeText(branchName)
   );
 
@@ -23,14 +23,15 @@ export const createButton = (branchName) => {
 
 export default createButton;
 
-const createSpan = () => {
-  const span = document.createElement("span");
-  span.setAttribute("aria-hidden", "true");
+const createInnerSpan = () => {
+  const span = document.createElement('span');
+  span.setAttribute('aria-hidden', 'true');
   span.setAttribute(
-    "class",
-    "left-icon flex-noshrink fabric-icon ms-Icon--Copy medium"
+    'class',
+    'left-icon flex-noshrink fabric-icon ms-Icon--Copy medium'
   );
   return span;
 };
 
-const buttonStyle = "padding: 8px; margin-left: .25rem; margin-right: .75rem;";
+const buttonStyle =
+  'padding: 4px; margin-left: .25rem; margin-right: .5rem; vertical-align: middle;';
--- page-pr.js
+++ page-pr.js
@@ -1,14 +1,26 @@
-import createButton, { buttonClassName } from "./button.js";
+import createButton, { buttonClassName } from './button.js';
 
-const headers = document.getElementsByClassName("pr-header-branches");
+const pattern = new RegExp(
+  /https:\/\/dev\.azure\.com\/.*\/_git\/.*\/pullrequest\/.*/
+);
 
-if (headers.length === 1) {
-  const links = headers[0].getElementsByTagName("a");
+const pageObserver = new MutationObserver(
+  () => pattern.test(location.href) && tryAddButtons()
+);
+pageObserver.observe(document, { subtree: true, childList: true });
 
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].getElementsByClassName(buttonClassName).length === 0) {
+function tryAddButtons() {
+  const headers = document.getElementsByClassName('pr-header-branches');
+
+  if (
+    headers.length === 1 &&
+    headers[0].getElementsByClassName(buttonClassName).length === 0
+  ) {
+    const links = headers[0].getElementsByTagName('a');
+
+    for (let i = 0; i < links.length; i++) {
       const button = createButton(links[i].innerHTML);
-      links[i].insertAdjacentElement("afterend", button);
+      links[i].insertAdjacentElement('afterend', button);
     }
   }
 }
